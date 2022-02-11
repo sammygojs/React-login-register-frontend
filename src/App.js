b8060f9e3bf84368af8270585451742fc99605f7
Sumit <akoliyasumit@hotmail.com>
2022-02-11 23:20:04 +0530

    login register React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [passw, setPassw] = useState('')
+  const [uname, setUname] = useState('')
+  
+  const fetchLogin = () =>{
+   Axios.post('http://localhost:3001/api/user/login',{email:email,password:passw})
+   .then((response) => alert(response.data)
+    , (error) => {
+    console.log(error);
+  })}
+
+  const fetchRegister = () =>{
+    Axios.post('http://localhost:3001/api/user/register',{name:uname,email:email,password:passw})
+    .then((response)=> alert(response.data.user)
+    , (error) => {
+      console.log(error);
+  })}
+  // Axios.get()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Login</h2>
+      <form method='POST'>
+        <label>Email</label>
+        <input type='email' id='uname' onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'></input>
+        <label>Password</label>
+        <input type='password' id='passw' onChange={(e)=>setPassw(e.target.value)} placeholder='Enter password'></input>
+        <button onClick={fetchLogin}>Log in</button>
+      </form>
+      <h2>Register</h2>
+      <form method='POST'>
+        <label>Username</label>
+        <input type='text' onChange={(e)=>setUname(e.target.value)} placeholder='Enter username'></input>
+        <label>Email</label>
+        <input type='email' onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email'></input>
+        <label>Password</label>
+        <input type='password' onChange={(e)=>setPassw(e.target.value)} placeholder='Enter password'></input>
+        <button onClick={fetchRegister}>Register</button>
+      </form>
     </div>
   );
 }
